dfc9bc5d1d4a652f4c2f6d33c26e93649470723f
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-19 15:58:23 +0300

    finish task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', onCurrentColor);
+
+function onCurrentColor(event) {
+  currentColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = currentColor.textContent; 
+}
+
+
+
