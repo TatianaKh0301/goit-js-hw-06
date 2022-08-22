c4099b45f2a57bfbfd5ea216109043540ad7bc58
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-22 07:21:33 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,14 @@
 const inputValidation = document.querySelector('#validation-input');
 
 const dataLength = Number(inputValidation.dataset.length);
+console.log("dataLength", dataLength);
 
 inputValidation.addEventListener('input', onInputValue);
 inputValidation.addEventListener('blur', onInputCheck);
 
 function onInputValue (event) {
     inputValidation.textContent = event.currentTarget.value;
+    console.log("inputValidation.textContent", inputValidation.textContent.length);
 };
 
 function onInputCheck (event) {
