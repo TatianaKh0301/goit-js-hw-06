e4a8855dbec1de15e330403e2a5858cc1c497272
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-22 07:06:34 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,37 +8,43 @@ const numberValueInput = document.querySelector('input');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
-console.log("boxes", boxesContainer);
+
 
 numberValueInput.addEventListener('input', onInputNumber);
 createBtn.addEventListener('click', onMakeBlocks);
+destroyBtn.addEventListener('click', destroyBoxes);
 
 function onInputNumber (event) {
   numberValueInput.textContent = event.currentTarget.value;
   inputNumber = Number(numberValueInput.textContent);
-  
 }
 
 const elements = [];
-function onMakeBlocks (event) {
-  for (let i = 0; i < inputNumber; i += 1) {
-    const blockEl = document.createElement('div');
-    blockEl.style.backgroundColor = 'red';
-    // blockEl.style.border = solid
-    elements.push(blockEl);
+function onMakeBlocks(event) {
+  event.preventDefault();
+  if (inputNumber === 0) {
+    alert('Нужно ввести число!!!!');
+  } else {
+    for (let i = 0, j = 30; i < inputNumber; i += 1, j += 10) {
+      const blockEl = document.createElement('div');
+      blockEl.classList.add = 'block';
+      blockEl.style.backgroundColor = getRandomHexColor();
+      const paddingSize = `${j}`;
+      blockEl.style.padding = `${paddingSize}px`;
+      elements.push(blockEl);
+    }
+  boxesContainer.append(...elements);
   }
-  return boxesContainer.append(...elements);
-  // console.log("elements", elements);
+  
 }
 
-// const makeblocks = blockAll => {
-//   return blocksAll.map(block => {
-//     const blockEl = document.createElement("div");
-//     blockEl.style.backgroundColor = getRandomHexColor(); 
-//     return blockEl;
-//   });
-// };
-
-// const elements = makeIngredients(ingredients);
-
-// ingrediendsList.append(...elements);
+function destroyBoxes(event) {
+  event.preventDefault();
+  const blockElementsDel = document.querySelectorAll('#boxes > div');
+  for (let i = 0; i < blockElementsDel.length; i += 1) {
+    blockElementsDel[i].remove();
+  }
+  numberValueInput.value = '';
+  elements.splice(0, blockElementsDel.length);
+  inputNumber = 0;
+}
